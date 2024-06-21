23124ac0246a9e8e2c2877046fd1fa4b6209ed07
Greenbox <greenboxer@list.ru>
2024-06-21 12:24:25 +0300

    [FEATURE] HMBM-666 front +back

--- src/scripts/aim.js
+++ src/scripts/aim.js
@@ -48,6 +48,8 @@ const hitTarget = () => {
 const endGame = () => {
   startButton.disabled = false;
   const averageTime = reactionTimes.reduce((a, b) => a + b, 0) / reactionTimes.length;
+  const forDb = Math.round(averageTime / 10) * 10
+  sendStatistics(forDb)
   const accuracy = maxTargets / clicksMade * 100;
   message.textContent = `Game Over! Average Reaction Time: 
   ${averageTime.toFixed(2)} ms, accuracy is ${(accuracy).toFixed(0)
@@ -64,3 +66,97 @@ const missClickOnTarget = (event) => {
 startButton.addEventListener('click', startGame);
 gameContainer.addEventListener('click', missClickOnTarget)
 
+
+function sendStatistics(accuracy) {
+  if (accuracy !== undefined) {
+    axios.post('http://83.166.237.173:3000/aim', { level: accuracy })
+      .then(response => {
+        console.log(response.data);
+        updateStatisticsChart();
+      })
+      .catch(error => console.error(error));
+  } else {
+    console.error('Error: accuracy is not defined');
+  }
+}
+
+function getStatistics() {
+  return axios.get('http://83.166.237.173:3000/aim')
+    .then(response => response.data)
+    .catch(error => console.error(error));
+}
+
+
+
+function updateStatisticsChart() {
+  console.log(getStatistics())
+  getStatistics().then(statistics => {
+    const labels = statistics.map(stat => stat.level);
+    const data = statistics.map(stat => stat.count);
+
+    const ctx = document.getElementById('statistics-chart').getContext('2d');
+    if (window.statisticsChart) {
+      window.statisticsChart.destroy();
+    }
+    window.statisticsChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [{
+          data,
+          backgroundColor: '#3284c8',
+          borderColor: '#3284c8',
+          borderWidth: 3,
+          fill: false,
+          pointRadius: 0,
+          pointHitRadius: 0,
+          tension: 0.4
+        }]
+      },
+      options: {
+        plugins: {
+          title: {
+            display: false
+          },
+          legend: {
+            display: false
+          }
+        },
+        scales: {
+          x: {
+            title: {
+              display: false,
+              text: 'Level',
+              color: 'white'
+            },
+            ticks: {
+              color: 'white'
+            },
+            grid: {
+              color: '#080808',
+              drawBorder: false,
+              drawTicks: false,
+              drawOnChartArea: true
+            }
+          },
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Count',
+              color: 'white'
+            },
+            ticks: {
+              color: 'white'
+            },
+            display: false
+          }
+        }
+      }
+    });
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  updateStatisticsChart()
+})
